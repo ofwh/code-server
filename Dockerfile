69d251dbb6d5bf5090d1bd05c5aa4f8571db630c
Larify <luoweihua7@gmail.com>
2023-07-18 12:42:25 +0800

    chore: 每日构建，将插件提前到打包

--- Dockerfile
+++ Dockerfile
@@ -31,6 +31,29 @@ RUN sudo chown -R coder:coder /home/coder/.local
 # Install a VS Code extension:
 # Note: we use a different marketplace than VS Code. See https://github.com/cdr/code-server/blob/main/docs/FAQ.md#differences-compared-to-vs-code
 # RUN code-server --install-extension esbenp.prettier-vscode
+RUN code-server --install-extension \
+  esbenp.prettier-vscode \
+  formulahendry.auto-close-tag \
+  formulahendry.auto-rename-tag \
+  mgmcdermott.vscode-language-babel \
+  michelemelluso.code-beautifier \
+  aaron-bond.better-comments \
+  formulahendry.code-runner \
+  streetsidesoftware.code-spell-checker \
+  dbaeumer.vscode-eslint \
+  mhutchie.git-graph \
+  eamodio.gitlens \
+  wix.vscode-import-cost \
+  yzhang.markdown-all-in-one \
+  zhuangtongfa.material-theme \
+  esbenp.prettier-vscode \
+  yoavbls.pretty-ts-errors \
+  richie5um2.vscode-sort-json \
+  bradlc.vscode-tailwindcss \
+  wayou.vscode-todo-highlight \
+  Vue.vscode-typescript-vue-plugin \
+  vscode-icons-team.vscode-icons \
+  Vue.volar
 
 # Install apt packages:
 # RUN sudo apt-get install -y ubuntu-make
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -24,28 +24,28 @@ if [ -d "${ENTRYPOINTD}" ]; then
   find "${ENTRYPOINTD}" -type f -executable -print -exec {} \;
 fi
 
-# Custom extensions
-/usr/bin/code-server --install-extension esbenp.prettier-vscode
-/usr/bin/code-server --install-extension formulahendry.auto-close-tag
-/usr/bin/code-server --install-extension formulahendry.auto-rename-tag
-/usr/bin/code-server --install-extension mgmcdermott.vscode-language-babel
-/usr/bin/code-server --install-extension michelemelluso.code-beautifier
-/usr/bin/code-server --install-extension aaron-bond.better-comments
-/usr/bin/code-server --install-extension formulahendry.code-runner
-/usr/bin/code-server --install-extension streetsidesoftware.code-spell-checker
-/usr/bin/code-server --install-extension dbaeumer.vscode-eslint
-/usr/bin/code-server --install-extension mhutchie.git-graph
-/usr/bin/code-server --install-extension eamodio.gitlens
-/usr/bin/code-server --install-extension wix.vscode-import-cost
-/usr/bin/code-server --install-extension yzhang.markdown-all-in-one
-/usr/bin/code-server --install-extension zhuangtongfa.material-theme
-/usr/bin/code-server --install-extension esbenp.prettier-vscode
-/usr/bin/code-server --install-extension yoavbls.pretty-ts-errors
-/usr/bin/code-server --install-extension richie5um2.vscode-sort-json
-/usr/bin/code-server --install-extension bradlc.vscode-tailwindcss
-/usr/bin/code-server --install-extension wayou.vscode-todo-highlight
-/usr/bin/code-server --install-extension Vue.vscode-typescript-vue-plugin
-/usr/bin/code-server --install-extension vscode-icons-team.vscode-icons
-/usr/bin/code-server --install-extension Vue.volar
+# # Custom extensions
+# /usr/bin/code-server --install-extension esbenp.prettier-vscode
+# /usr/bin/code-server --install-extension formulahendry.auto-close-tag
+# /usr/bin/code-server --install-extension formulahendry.auto-rename-tag
+# /usr/bin/code-server --install-extension mgmcdermott.vscode-language-babel
+# /usr/bin/code-server --install-extension michelemelluso.code-beautifier
+# /usr/bin/code-server --install-extension aaron-bond.better-comments
+# /usr/bin/code-server --install-extension formulahendry.code-runner
+# /usr/bin/code-server --install-extension streetsidesoftware.code-spell-checker
+# /usr/bin/code-server --install-extension dbaeumer.vscode-eslint
+# /usr/bin/code-server --install-extension mhutchie.git-graph
+# /usr/bin/code-server --install-extension eamodio.gitlens
+# /usr/bin/code-server --install-extension wix.vscode-import-cost
+# /usr/bin/code-server --install-extension yzhang.markdown-all-in-one
+# /usr/bin/code-server --install-extension zhuangtongfa.material-theme
+# /usr/bin/code-server --install-extension esbenp.prettier-vscode
+# /usr/bin/code-server --install-extension yoavbls.pretty-ts-errors
+# /usr/bin/code-server --install-extension richie5um2.vscode-sort-json
+# /usr/bin/code-server --install-extension bradlc.vscode-tailwindcss
+# /usr/bin/code-server --install-extension wayou.vscode-todo-highlight
+# /usr/bin/code-server --install-extension Vue.vscode-typescript-vue-plugin
+# /usr/bin/code-server --install-extension vscode-icons-team.vscode-icons
+# /usr/bin/code-server --install-extension Vue.volar
 
 exec dumb-init /usr/bin/code-server "$@" --disable-telemetry
